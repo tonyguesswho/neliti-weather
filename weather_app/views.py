864f7b47bd5337f70bc805bac8f50d45e72323c8
Anthony Ugwu <anthonyugwu234@gmail.com>
2021-06-01 15:48:21 +0100

    access api and fix 403 issue

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+import requests
+import json 
 
 def index(request):
-    return render(request, 'weather_app/index.html') 
+	url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=-0.25'
+
+	try:
+		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+		res = requests.get(url , timeout=60, headers=headers)
+	except requests.ConnectionError as e:
+		raise Exception(str(e))
+	except Exception as e:
+		raise Exception(str(e))
+
+	if not 200 <= res.status_code < 300:
+		raise Exception
+	else:
+		try:
+			latest_data = res.json()['properties']['timeseries'][0]['data']['instant']['details']
+			print(latest_data)
+		except KeyError as e:
+			latest_data ={}
+
+
+	return render(request, 'weather_app/index.html') #returns the index.html template
